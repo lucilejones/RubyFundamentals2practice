a2afe11db77588b46ee896744ce3f1b10aaef4d1
lucilejones <99698725+lucilejones@users.noreply.github.com>
2024-01-11 03:43:51 +0000

    Finished all but last practice exercises and tests

--- main.rb
+++ main.rb
@@ -88,3 +88,56 @@ dog2 = Dog2.new("Peanut")
 dog2.print_details
 # dog2.print_details_private
 
+
+# Define a class called 'Car' that takes a make and model as arguments.
+# Define a method called 'print_color' that prints the color of the car.
+# Create an instance of the 'Car' class called 'car'.
+# Create a test for the 'Car' class that tests the 'print_color' method.
+
+class Car
+  def initialize(make, model, color)
+    @make = make
+    @model = model
+    @color = color
+  end
+
+  def print_color
+    puts @color
+  end
+end
+
+
+# Define a class called 'Fruit' that takes a name as an argument.
+# Define a method called 'print_name' that prints the name of the fruit.
+# Define a class called 'Apple' that inherits from the 'Fruit' class.
+# Create an instance of the 'Apple' class called 'apple'.
+# Create a test for the 'Apple' class that tests the 'print_name' method.
+
+class Fruit
+  def initialize(name)
+    @name = name
+  end
+
+  def print_name
+    puts @name
+  end
+end
+
+class Apple < Fruit
+end
+
+apple = Apple.new("new apple")
+apple.print_name
+
+
+# Define a Car class:
+
+# The class should initialize with a make, model, and color. Include methods to get and set each of these attributes.
+# Add a method info that returns a string with all the car's details.
+# Create a Garage class:
+
+# This class will manage a collection of Car objects.
+# Implement methods to add a car, remove a car by make and model, and list all cars.
+# Add a method to find all cars of a specific color.
+# add a method to clear all cars from the garage.
+
